fc004eace7b58c03f0266f2bed3e4c0a06912426
Roger Marrugat Escobedo <Roger Marrugat Escobedo>
2016-12-08 01:59:25 +0100

    Afegit javascript pel buscador (Ignorar el iframe)

--- javascript.js
+++ javascript.js
@@ -1,23 +1,68 @@
 
 (function(){
 
+
+	var MusicRecommender = {
+		search : function search (query) {
+				var url = "https://api.spotify.com/v1/search?q=" + query + "&type=album,track,artist&market=" + country + "&limit=" + limit_results;
+				var array = JSON.parse(AJAX.request(url));
+				console.log(array);
+				//this.getAlbum(array.albums.items[0].id);
+				//this.getArtist(array.artists.items[0].id);
+		},
+
+		getAlbum : function getAlbum (id_album) {
+			var url = "https://api.spotify.com/v1/albums/" + id_album + "?market=" + country;
+			var array = JSON.parse(AJAX.request(url));
+			console.log(array);
+		},
+
+		getArtist : function getArtist (id_artist) {
+			var url = "	https://api.spotify.com/v1/artists/" + id_artist + "/albums?album_type=single,album&market=" + country + "&limit=" + limit_results;
+			var array = JSON.parse(AJAX.request(url));
+			console.log(array);
+		}
+	}
+
+	var AJAX = {
+		request: function request(url){
+			var xhr = new XMLHttpRequest();
+			xhr.open("GET", url, false);
+			xhr.send();
+			return xhr.responseText;
+		}
+	}
+
+	var Listener = {
+		addListener : function addListener (target, event, callback, capture) {
+			target.addEventListener(event, callback, capture);
+		},
+		eventSearch: function eventPlay(event){
+			event.preventDefault();
+
+			var query = document.getElementById("search").value
+
+			if(!query){
+				alert("Your search is short.");
+				return false
+			}
+
+			MusicRecommender.search(query);
+		}
+	}
+
 	var Application = {
 		start: function start(){
+
+			var button = document.getElementById('action-search');
+			Listener.addListener (button, 'click', Listener.eventSearch, false);
 
 		}
 	}
 
-	Listener.add(
-		document,
-		"DOMContentLoaded",
-		Application.start(
-			google_api_key,
-			country,
-			limit_results,
-			0
-		),
-		false
-	);
+	var limit_results = 10;
+	var country = "ES";
+
+	Listener.addListener(document, "DOMContentLoaded", Application.start(spotify_api_key, 0), false);
+
 }());
-
-
--- spotify_api_data.js
+++ spotify_api_data.js
@@ -1,1 +1,1 @@
-var sptify_api_key = "e025e9bb09aa80d125e4ef990d992f6e";
+var spotify_api_key = "e025e9bb09aa80d125e4ef990d992f6e";
